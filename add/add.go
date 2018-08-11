550c7beed367a38d4cf2873ce6efa41ff0b08545
amifork <amifork@gmail.com>
2018-08-11 02:37:46 +0000

    sub.goを追加

--- add/add.go
+++ add/add.go
@@ -2,5 +2,5 @@ package add
 
 // Add x + yの値を返す。
 func Add(x, y int) int {
-	return x - y
+	return x + y
 }
---
+++ sub/sub.go
@@ -0,0 +1,6 @@
+package sub
+
+// Sub x - yの値を返す。
+func Sub(x, y int) int {
+	return x - y
+}

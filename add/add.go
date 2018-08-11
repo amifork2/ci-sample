0eb6505b5e7e3ea0716639cf73fb8e9d0d90670e
amifork <amifork@gmail.com>
2018-08-11 11:19:53 +0000

    わざと失敗させる

--- add/add.go
+++ add/add.go
@@ -1,6 +1,129 @@
 package add
 
-// Add x + yの値を返す。
 func Add(x, y int) int {
 	return x + y
+}
+
+func f1() int {
+	return 0
+}
+
+func f2() int {
+	return 0
+}
+
+func f3() int {
+	return 0
+}
+
+func f4() int {
+	return 0
+}
+
+func f5() int {
+	return 0
+}
+
+func f6() int {
+	return 0
+}
+
+func f7() int {
+	return 0
+}
+
+func f8() int {
+	return 0
+}
+
+func f9() int {
+	return 0
+}
+
+func f10() int {
+	return 0
+}
+
+func f11() int {
+	return 0
+}
+
+func f12() int {
+	return 0
+}
+
+func f13() int {
+	return 0
+}
+
+func f14() int {
+	return 0
+}
+
+func f15() int {
+	return 0
+}
+
+func f16() int {
+	return 0
+}
+
+func f17() int {
+	return 0
+}
+
+func f18() int {
+	return 0
+}
+
+func f19() int {
+	return 0
+}
+
+func f20() int {
+	return 0
+}
+
+func f21() int {
+	return 0
+}
+
+func f22() int {
+	return 0
+}
+
+func f23() int {
+	return 0
+}
+
+func f24() int {
+	return 0
+}
+
+func f25() int {
+	return 0
+}
+
+func f26() int {
+	return 0
+}
+
+func f27() int {
+	return 0
+}
+
+func f28() int {
+	return 0
+}
+
+func f29() int {
+	return 0
+}
+
+func f30() int {
+	return 0
+}
+
+func f31() int {
+	return 0
 }
--- sub/sub.go
+++ sub/sub.go
@@ -1,6 +1,5 @@
 package sub
 
-// Sub x - yの値を返す。
 func Sub(x, y int) int {
-	return x - y
+	return x + y
 }
